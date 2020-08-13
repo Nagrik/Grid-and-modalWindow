ff423dabef31b6d6a18e4bf2e9d250e96ff66ba6
RomanNahryshko <nagrishkoo@gmail.com>
2020-08-14 00:31:13 +0300

    ModalWithOutJs

---
+++ index.js
@@ -0,0 +1,14 @@
+$('[data-fancybox="#modal"]').fancybox({
+    buttons : [ 
+        'slideShow',
+        'share',
+        'zoom',
+        'fullScreen',
+        'download',
+        'close'
+      ],
+    animationEffect: "zoom-in-out",
+    animationDuration: 1600,
+    transitionEffect: "rotate",
+    transitionDuration: 1400,
+    })
